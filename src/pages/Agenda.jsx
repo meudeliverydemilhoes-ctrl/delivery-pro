35752a1c3287026ae359dca1b5bc935a1f7f85d2
Meu delivery de milhões <meudeliverydemilhoes@gmail.com>
2025-12-09 03:13:52 +0000

    File changes

--- src/pages/Agenda.jsx
+++ src/pages/Agenda.jsx
@@ -162,7 +162,7 @@ export default function Agenda() {
         </div>
         <div className="flex gap-2">
           <Link to={createPageUrl("Dashboard")}>
-            <Button variant="outline" className="border-white/10 text-white">
+            <Button className="bg-[#FF4D00] hover:bg-[#E64500] text-white">
               <Home size={18} className="mr-2" /> Início
             </Button>
           </Link>
