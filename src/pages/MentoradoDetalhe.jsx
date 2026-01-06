5a03591e09021e727ad76e24bf60c597c616af84
Meu delivery de milhões <meudeliverydemilhoes@gmail.com>
2026-01-06 02:02:51 +0000

    File changes

--- src/pages/MentoradoDetalhe.jsx
+++ src/pages/MentoradoDetalhe.jsx
@@ -616,7 +616,7 @@ export default function MentoradoDetalhe() {
                   });
                 }
               }}
-            />
+              />
             </div>
           </>
         )}
